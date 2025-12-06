416ed3a28144adfe54e92c2c09e30bcf451452d8
BegovicNikola <begovic.office@gmail.com>
2025-12-06 03:26:05 +0100

    materialui theme setup

---
+++ src/lib/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+// TODO: Add ligth & dark theme options
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import { createRoot } from "react-dom/client";
 
 import App from "@/App.tsx";
 import { queryClient } from "@/lib/queryClient";
+import { theme } from "@/lib/theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
